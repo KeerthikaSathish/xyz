d09c1fc2e2f656c9ff68de12dfc52851de2514c6
keerthi_ka <80771780+KeerthikaSathish@users.noreply.github.com>
2023-05-16 22:01:58 +0530

    Create .py

---
+++ .py
@@ -0,0 +1,10 @@
+import IPython
+from scipy.io import wavfile
+import noisereduce as nr
+import soundfile as sf
+from noisereduce.generate_noise import band_limited_noise
+import matplotlib.pyplot as plt
+import urllib.request
+import numpy as np
+import io
+%matplotlib inline
